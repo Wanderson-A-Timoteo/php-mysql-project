64306e36ca90fa1122de74f19ca6964e2c84acbd
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-10-03 18:00:45 -0400

    Inicialização do projeto em PHP - Ma Belle, criado index.php e style.css

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Ma Belle | Página Inicial</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+    </head>
+    <body>
+        <div id="box">
+            <div id="barra_top">
+            
+            </div> <!-- Final da div barra_top -->
+            <div id="header">
+
+            </div> <!-- Final da div header -->
+            <div id="search">
+            
+            </div> <!-- Final da div de pesquisa search -->
+        </div> <!-- Final da div box -->
+    </body>
+</html>
