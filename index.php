8fc04b38817120e0a454161b24004463c3604911
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-10-03 21:28:01 -0400

    add images e novas modificações no index e css da página inicial

--- index.php
+++ index.php
@@ -3,16 +3,31 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Ma Belle | Página Inicial</title>
+        <title>Ma Belle Lingerie | Página Inicial</title>
         <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
     </head>
     <body>
         <div id="box">
             <div id="barra_top">
-            
-            </div> <!-- Final da div barra_top -->
-            <div id="header">
+                <span id="horas">Hoje, 03 de Outubro de 2020 ás 20:58</span>
+                <img src="images/check-mark.svg" alt="Imagem de check" border="0" />
+                <a href="#">Meus Pedidos</a>
 
+                <img src="images/customer-service.svg" alt="Imagem de atendimento" border="0" />
+                <a href="#">Atendimento</a>
+
+                <img src="images/carts.svg" alt="Imagem carrinho de compras" border="0" />
+                <a href="#">(3) Produtos</a>
+            </div> <!-- Final da div barra_top -->
+
+            <div id="header">
+                <div class="wellcome">
+                    <img src="images/user-woman.svg" alt="Imagem de Usuário" id="user" border="0" />
+                    <span>Olá visitante, <a href="#"> Cadastre-se </a> ou <a href="#"> Faça Login </a></span>
+                </div> <!-- Final da div wellcome -->
+                <a href="#" title="Ma Belle Lingerie - Logomarca">
+                    <img src="images/Logomarca.png" alt="Ma Belle Lingerie - Logomarca" id="logomarca" border="0"/>
+                </a>
             </div> <!-- Final da div header -->
             <div id="search">
             
