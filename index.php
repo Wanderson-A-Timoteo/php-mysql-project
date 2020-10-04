febdd0105edfc1d3f88eb39ec50a90635739efbf
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-10-04 18:57:26 -0400

    Modificado toda a página inicial, add menu, logomarca de cabeçalho, sidebar de categorias e footer

--- index.php
+++ index.php
@@ -1,37 +1,51 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Ma Belle Lingerie | Página Inicial</title>
-        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
-    </head>
-    <body>
-        <div id="box">
-            <div id="barra_top">
-                <span id="horas">Hoje, 03 de Outubro de 2020 ás 20:58</span>
-                <img src="images/check-mark.svg" alt="Imagem de check" border="0" />
-                <a href="#">Meus Pedidos</a>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Belle Lingerie | Página Inicial</title>
+    <link rel="stylesheet" href="styles/style.css" media="all" />
+</head>
+<body>
+    <div class="main_wrapper">
+        <div class="header_wrapper">
+        <img src="images/logomarca.png" alt="Logomarca" id="logo">
+        </div>
+        
+        <!--Cabeçalho -->
+        <div class="menubar">
+            <ul id="menu">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Todos Produtos</a></li>
+                <li><a href="#">Minha Conta</a></li>
+                <li><a href="#">Carrinho</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+            <div id="form"> 
+                <form action="result.php" method="get" enctype="multpart/form-data">
+                <input type="text" name="user_query" id="" placeholder="Pesquisar Produtos">
+                <input type="submit" value="Pesquisar" name="search">
+                </form>
+            </div>
+        </div>
 
-                <img src="images/customer-service.svg" alt="Imagem de atendimento" border="0" />
-                <a href="#">Atendimento</a>
-
-                <img src="images/carts.svg" alt="Imagem carrinho de compras" border="0" />
-                <a href="#">(3) Produtos</a>
-            </div> <!-- Final da div barra_top -->
-
-            <div id="header">
-                <div class="wellcome">
-                    <img src="images/user-woman.svg" alt="Imagem de Usuário" id="user" border="0" />
-                    <span>Olá visitante, <a href="#"> Cadastre-se </a> ou <a href="#"> Faça Login </a></span>
-                </div> <!-- Final da div wellcome -->
-                <a href="#" title="Ma Belle Lingerie - Logomarca">
-                    <img src="images/Logomarca.png" alt="Ma Belle Lingerie - Logomarca" id="logomarca" border="0"/>
-                </a>
-            </div> <!-- Final da div header -->
-            <div id="search">
-            
-            </div> <!-- Final da div de pesquisa search -->
-        </div> <!-- Final da div box -->
-    </body>
+        <div class="content_wrapper">
+            <div id="sidebar">
+                <div id="sidebar_title"> Categorias: </div>
+                    <ul id="cats">
+                        <li><a href="#">Calcinha</a></li>
+                        <li><a href="#">Sutiã</a></li>
+                        <li><a href="#">Body</a></li>
+                        <li><a href="#">Conjunto</a></li>
+                        <li><a href="#">Baby Doll</a></li>
+                        <li><a href="#">Camisolas</a></li>
+                        <li><a href="#">Cueca Box</a></li>
+                        <li><a href="#">Samba Canção</a></li>
+                    </ul>
+                </div>
+            <div id="content_area">content area</div>
+            <div id="footer">rodape</div>
+        </div>
+    </div>
+</body>
 </html>
